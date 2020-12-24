ce4e931d8b32119fdc80ab1cb02821627ba21c86
Alexander Kachkaev <alexander@kachkaev.ru>
2020-12-24 17:34:38 +0000

    Simplify

--- server.js
+++ server.js
@@ -1,42 +1,71 @@
+// @ts-check
+
 const http = require("http");
-const compression = require("compression");
 
-const applyCompression = compression();
+const generatePayload = (size) =>
+  Array.from(Array(size))
+    .map(() => `${Math.random()}`[2])
+    .join("");
 
-const defaultContentSize = 1024;
+const assertWriteResult = (result, expectedValue) => {
+  if (!expectedValue && result) {
+    throw new Error(
+      "res.write unexpectedly returned true – try decreasing payload size to avoid this"
+    );
+  } else if (expectedValue && !result) {
+    throw new Error(
+      "res.write unexpectedly returned false – try increasing payload size to avoid this"
+    );
+  }
+};
+
 http
   .createServer(function (req, res) {
     const url = new URL(req.url, "http://localhost:8080");
 
-    if (process.env.COMPRESSION === "true") {
-      applyCompression(req, res, () => {});
-    }
-
     switch (url.pathname) {
       case "/redirect": {
-        const toSize = parseInt(url.searchParams.get("toSize"));
-        res.writeHead(302, {
-          Location: `/content?size=${toSize > 0 ? toSize : defaultContentSize}`,
-        });
-        res.end();
+        const writeEmptyBody =
+          url.searchParams.get("writeEmptyBody") === "true";
+        const endTwice = url.searchParams.get("endTwice") === "true";
+        const contentType = url.searchParams.get("contentType");
 
-        if (process.env.DOUBLE_END === "true") {
+        res.writeHead(302, {
+          Location: `/content?type=${contentType}`,
+        });
+
+        if (writeEmptyBody) {
+          res.write("");
+        }
+
+        res.end();
+        if (endTwice) {
           res.end();
         }
+
         return;
       }
+
       case "/content": {
-        const size = parseInt(url.searchParams.get("size"));
-        const payload = Array.from(Array(size > 0 ? size : defaultContentSize))
-          .map(() => `${Math.random()}`[2])
-          .join("");
+        const type = url.searchParams.get("type");
 
-        res.writeHead(200, {
-          "Content-Type": "text/plain",
-        });
+        if (type === "withDrain") {
+          res.writeHead(200, {
+            "Content-Type": "text/plain",
+            "Transfer-Encoding": "chunked",
+          });
 
-        res.write(payload);
-        res.end();
+          assertWriteResult(res.write(generatePayload(1024 * 100)), false);
+
+          res.on("drain", () => {
+            assertWriteResult(res.write(generatePayload(1024)), true);
+            res.end();
+          });
+        } else {
+          assertWriteResult(res.write(generatePayload(1024)), true);
+          res.end();
+        }
+
         return;
       }
     }
